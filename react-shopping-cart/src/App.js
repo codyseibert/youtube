3c815c0b7f2573512648da2322325e5376175b62
Cody Seibert <cseibert113@gmail.com>
2020-07-10 10:25:29 -0400

    shopping cart

--- react-shopping-cart/src/App.js
+++ react-shopping-cart/src/App.js
@@ -10,25 +10,22 @@ function App() {
   const [cart, setCart] = useState([]);
   const [page, setPage] = useState(PAGE_PRODUCTS);
 
-  const removeFromCart = (productToRemove) => {
-    setCart(
-      cart.filter((product) => product !== productToRemove)
-    );
-  };
-
-  const addToCart = (product) => {
-    setCart([...cart, { ...product }]);
-  };
-
   const navigateTo = (nextPage) => {
     setPage(nextPage);
+  };
+
+  const getCartTotal = () => {
+    return cart.reduce(
+      (sum, { quantity }) => sum + quantity,
+      0
+    );
   };
 
   return (
     <div className="App">
       <header>
         <button onClick={() => navigateTo(PAGE_CART)}>
-          Go to Cart ({cart.length})
+          Go to Cart ({getCartTotal()})
         </button>
 
         <button onClick={() => navigateTo(PAGE_PRODUCTS)}>
@@ -36,10 +33,10 @@ function App() {
         </button>
       </header>
       {page === PAGE_PRODUCTS && (
-        <Products addToCart={addToCart} />
+        <Products cart={cart} setCart={setCart} />
       )}
       {page === PAGE_CART && (
-        <Cart cart={cart} removeFromCart={removeFromCart} />
+        <Cart cart={cart} setCart={setCart} />
       )}
     </div>
   );
