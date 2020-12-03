341d0bda943452572bc850f82b1848ae3a7bd2bf
Cody Seibert <cseibert@flexion.us>
2020-12-03 09:36:27 -0500

    web crawler

--- web-crawler-node/src/index.ts
+++ web-crawler-node/src/index.ts
@@ -1,71 +1,58 @@
 import * as fetch from "node-fetch";
 import * as cheerio from "cheerio";
+import * as fs from "fs";
 import * as path from "path";
 import * as urlParser from "url";
-import * as fs from "fs";
 
-const getFullUrl = ({ url, host, protocol }) => {
-  let fullUrl = null;
-  if (url.includes("http")) {
-    fullUrl = url;
-  } else if (url.startsWith("/")) {
-    fullUrl = `${protocol}//${host}${url}`;
+const seenUrls = {};
+
+const getUrl = (link, host, protocol) => {
+  if (link.includes("http")) {
+    return link;
+  } else if (link.startsWith("/")) {
+    return `${protocol}//${host}${link}`;
   } else {
-    fullUrl = `${protocol}//${host}/${url}`;
+    return `${protocol}//${host}/${link}`;
   }
-  return fullUrl.replace(/\/$/, "");
 };
 
-const downloadImage = (imageUrl) =>
-  fetch(imageUrl)
-    .then((res) => {
-      const filename = path.basename(imageUrl);
-      const dest = fs.createWriteStream(path.join("./images", filename));
-      res.body.pipe(dest);
-    })
-    .catch((err) => console.log(err));
-
-const downloadImages = ({ $images, host, protocol }) => {
-  return Promise.all(
-    $images
-      .map((i, $link) => $link.attribs.src)
-      .map((i, src) => getFullUrl({ url: src, host, protocol }))
-      .map((i, imageUrl) => downloadImage(imageUrl))
-      .get()
-  );
-};
-
-const crawlLinks = ({ $links, host, protocol, ignoreUrlWith, seenUrls }) => {
-  return Promise.all(
-    $links
-      .map((i, $link) => $link.attribs.href)
-      .map((i, href) => getFullUrl({ url: href, host, protocol }))
-      .filter((i, href) => href.includes(host))
-      .map((i, url) => crawl({ url, ignoreUrlWith, seenUrls }))
-      .get()
-  );
-};
-
-const crawl = async ({ url, ignoreUrlWith, seenUrls }) => {
-  if (ignoreUrlWith.some((ignore) => url.includes(ignore))) return;
+const crawl = async ({ url, ignore }) => {
   if (seenUrls[url]) return;
+  console.log("crawling", url);
   seenUrls[url] = true;
-
-  console.log(`crawling ${url}`);
 
   const { host, protocol } = urlParser.parse(url);
 
-  const html = await fetch(url).then((res) => res.text());
+  const response = await fetch(url);
+  const html = await response.text();
   const $ = cheerio.load(html);
-  const $images = $("img");
-  const $links = $("a");
+  const links = $("a")
+    .map((i, link) => link.attribs.href)
+    .get();
 
-  await downloadImages({ $images, host, protocol });
-  await crawlLinks({ $links, host, protocol, ignoreUrlWith, seenUrls });
+  const imageUrls = $("img")
+    .map((i, link) => link.attribs.src)
+    .get();
+
+  imageUrls.forEach((imageUrl) => {
+    fetch(getUrl(imageUrl, host, protocol)).then((response) => {
+      const filename = path.basename(imageUrl);
+      const dest = fs.createWriteStream(`images/${filename}`);
+      response.body.pipe(dest);
+    });
+  });
+
+  links
+    .filter((link) => link.includes(host) && !link.includes(ignore))
+    .forEach((link) => {
+      crawl({
+        url: getUrl(link, host, protocol),
+        ignore,
+      });
+    });
 };
 
 crawl({
-  url: "http://stevescooking.blogspot.com",
-  ignoreUrlWith: ["/search?"],
-  seenUrls: {},
+  url: "http://stevescooking.blogspot.com/",
+  ignore: "/search",
 });
